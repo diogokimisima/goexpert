401f4a26b432a462ede1d5d4e8aac716bad14010
DIOGOKIMISIMA\diogo <diogo2.trovata@gmail.com>
2024-12-30 10:48:14 -0300

    pegadinha has many

---
+++ banco-de-dados/6/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID       int `gorm:"column:id;primaryKey"`
+	Name     string
+	Products []Product
+}
+
+type Product struct {
+	ID           int `gorm:"column:id;primaryKey"`
+	Name         string
+	Price        float64
+	CategoryID   int
+	Category     Category
+	SerialNumber SerialNumber
+	gorm.Model
+}
+
+type SerialNumber struct {
+	ID        int `gorm:"column:id;primaryKey"`
+	Number    string
+	ProductID int
+}
+
+func main() {
+	// Adicionando parseTime=true ao DSN
+	dsn := "root:root@tcp(localhost:3306)/goexpert?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{}, &SerialNumber{})
+
+	category := Category{Name: "Eletrônicos"}
+	db.Create(&category)
+
+	db.Create(&Product{
+		Name:       "Mouse 2",
+		Price:      1000.00,
+		CategoryID: 1, // Ajustando para usar o ID do objeto criado
+	})
+
+	db.Create(&SerialNumber{
+		Number:    "123456",
+		ProductID: 1,
+	})
+
+	var products []Product
+	db.Preload("Category").Find(&products)
+	for _, product := range products {
+		println(product.Name, product.Category.Name)
+	}
+
+	var categories []Category
+	err = db.Model(&Category{}).Preload("Products.SerialNumber").Find(&categories).Error
+	if err != nil {
+		panic(err)
+	}
+	for _, category := range categories {
+		println(category.Name)
+		for _, product := range category.Products {
+			println("-", product.Name, product.SerialNumber.Number)
+		}
+	}
+
+}
