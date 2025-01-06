a14b037d83d5e27bea020732579d1f563eb8acdf
DIOGOKIMISIMA\diogo <diogo2.trovata@gmail.com>
2025-01-06 10:47:02 -0300

    criando testes com testify

--- 6-testing/2/tax.go
+++ 6-testing/2/tax.go
@@ -1,27 +1,16 @@
 package tax
 
-import "time"
+import "errors"
 
-func CalculateTax(amount float64) float64 {
+func CalculateTax(amount float64) (float64, error) {
 	if amount <= 0 {
-		return 0
+		return 0.0, errors.New("amount must be greater than 0")
 	}
 	if amount >= 1000 && amount < 20000 {
-		return 10.0
+		return 10.0, nil
 	}
 	if amount >= 20000 {
-		return 20.0
+		return 20.0, nil
 	}
-	return 5.0
-}
-
-func CalculateTax2(amount float64) float64 {
-	time.Sleep(time.Millisecond)
-	if amount <= 0 {
-		return 0
-	}
-	if amount >= 1000 {
-		return 10.0
-	}
-	return 5.0
+	return 5.0, nil
 }
