28f868f9b3dc5d147536f7c64f511a58262573e4
diogokimisima <dkimisima@gmail.com>
2025-01-14 17:52:43 -0300

    criando a rota do swagger de docs

--- 7-APIS/cmd/server/main.go
+++ 7-APIS/cmd/server/main.go
@@ -4,16 +4,35 @@ import (
 	"net/http"
 
 	"github.com/diogokimisima/goexpert/7-APIS/configs"
+	_ "github.com/diogokimisima/goexpert/7-APIS/docs"
 	"github.com/diogokimisima/goexpert/7-APIS/internal/entity"
 	"github.com/diogokimisima/goexpert/7-APIS/internal/infra/database"
 	"github.com/diogokimisima/goexpert/7-APIS/internal/infra/webserver/handlers"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/jwtauth"
+	httpSwager "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+// @title 			Go Expert API Example
+// @version 	   	1.0
+// @description 	Product API with authentication
+// @termsOfService 	http://swagger.io/terms/
+
+// @contact.name 	Diogo Kimisima
+// @contact.url 	http://www.diogokimisima.com.br
+// @contact.email 	dkimisima@gmail.com
+
+// @license.name 	Full Cycle License
+// @license.url 	http://www.fullcycle.com.br
+
+// @host 			localhost:8000
+// @BasePath 		/
+// @securityDefinitions.apikey ApiKeyAuth
+// @in 				header
+// @name 			Authorization
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
@@ -48,6 +67,8 @@ func main() {
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate_token", userHandler.GetJwt)
+
+	r.Get("/docs/*", httpSwager.Handler(httpSwager.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
