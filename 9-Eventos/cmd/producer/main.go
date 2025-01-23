347e76757ce868a42e73608ffbdfeb8cbf0ae57a
diogokimisima <dkimisima@gmail.com>
2025-01-23 18:03:14 -0300

    publicando e consumindo uma mensagem

---
+++ 9-Eventos/cmd/producer/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/diogokimisima/fcutils/pkg/rabbitmq"
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	rabbitmq.Publish(ch, "Hello World!", "amq.direct")
+}
