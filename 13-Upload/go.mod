103c712284e58add6b16ecf2a9c43f30b7e5bdb4
diogokimisima <dkimisima@gmail.com>
2025-02-06 16:24:35 -0300

    upload simultaneos

---
+++ 13-Upload/go.mod
@@ -0,0 +1,7 @@
+module github.com/diogokimisima/goexpert/13-upload
+
+go 1.23.3
+
+require github.com/aws/aws-sdk-go v1.55.6
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ 13-Upload/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+	wg       sync.WaitGroup
+)
+
+func init() {
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"", // credential
+				"", // secret
+				"", // token
+			),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	s3Client = s3.New(sess)
+	s3Bucket = "goexpert-bucket-exemplo"
+}
+
+func main() {
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+	uploadControll := make(chan struct{}, 100)
+	for {
+		files, err := dir.Readdir(1)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error reading directory: %s\n", err)
+			uploadControll <- struct{}{}
+			continue
+		}
+		wg.Add(1)
+		uploadControll <- struct{}{}
+		go uploadFile(files[0].Name(), uploadControll)
+	}
+	wg.Wait()
+}
+
+func uploadFile(filename string, uploadControll <-chan struct{}) {
+	completeFileName := fmt.Sprintf("./tmp/%s", filename)
+	fmt.Printf("Uploading file %s to bucket %s\n", completeFileName, s3Bucket)
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opening file %s", completeFileName)
+		<-uploadControll // esvazia o canal
+		return
+	}
+	defer f.Close()
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(filename),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("Error uploading file %s", completeFileName)
+		<-uploadControll // esvazia o canal
+		return
+	}
+	fmt.Printf("File %s uploaded successfully", completeFileName)
+	<-uploadControll // esvazia o canal
+}
