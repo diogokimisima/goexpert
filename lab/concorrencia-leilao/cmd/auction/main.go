0274e02d84cc83779290d70066f4e09297d34e2f
diogokimsiima <dkimisima@gmail.com>
2025-12-09 19:06:52 -0300

    conectando o projto com banco mongodb

--- lab/concorrencia-leilao/cmd/auction/main.go
+++ lab/concorrencia-leilao/cmd/auction/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	"github.com/diogokimisima/fullcycle-auction/configuration/database/mongodb"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
+	ctx := context.Background()
+
+	if err := godotenv.Load("cmd/auction/.env"); err != nil {
 		log.Fatal("Error loading .env file")
 		return
 	}
 
+	databaseClient, err := mongodb.NewMongoDBConnection(ctx)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+		return
+	}
+
+	log.Println("Successfully connected to MongoDB:", databaseClient.Name())
 }
