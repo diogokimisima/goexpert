8ac104f1b2eab707e0190b56214ac70b768cff4d
diogokimsiima <dkimisima@gmail.com>
2025-12-15 12:20:31 -0300

    feat: implement user retrieval with error handling and internal error types

--- lab/concorrencia-leilao/internal/infra/database/user/find_user.go
+++ lab/concorrencia-leilao/internal/infra/database/user/find_user.go
@@ -1,6 +1,14 @@
 package user
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/diogokimisima/fullcycle-auction/configuration/logger"
+	"github.com/diogokimisima/fullcycle-auction/internal/entity/user_entity"
+	"github.com/diogokimisima/fullcycle-auction/internal/internal_error"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,8 +27,27 @@ func NewUserRepository(database *mongo.Database) *UserRepository {
 	}
 }
 
-// func (ur *UserRepository) FindUserById(
-// 	ctx context.Context, userId string) (*user_entity.User, error) {
-// 	filter := bson.M{"_id": userId}
+func (ur *UserRepository) FindUserById(
+	ctx context.Context, userId string) (*user_entity.User, error) {
+	filter := bson.M{"_id": userId}
 
-// }
+	var userEntityMongo UserEntityMongo
+	err := ur.Collection.FindOne(ctx, filter).Decode(&userEntityMongo)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			errMsg := fmt.Sprintf("user with id %s not found", userId)
+			logger.Error(errMsg, mongo.ErrNoDocuments)
+			return nil, internal_error.NewNotFoundError(errMsg)
+		}
+
+		logger.Error(fmt.Sprintf("Error trying to find user by userId %s: %v", userId, err), err)
+		return nil, internal_error.NewInternalServerError(fmt.Sprintf("Error trying to find user by userId %s: %v", userId, err))
+	}
+
+	userEntity := &user_entity.User{
+		Id:   userEntityMongo.Id,
+		Name: userEntityMongo.Name,
+	}
+
+	return userEntity, nil
+}
---
+++ lab/concorrencia-leilao/internal/internal_error/internal_error.go
@@ -0,0 +1,24 @@
+package internal_error
+
+type InternalError struct {
+	Message string
+	Err     string
+}
+
+func (e *InternalError) Error() string {
+	return e.Message
+}
+
+func NewNotFoundError(message string) *InternalError {
+	return &InternalError{
+		Message: message,
+		Err:     "not_found",
+	}
+}
+
+func NewInternalServerError(message string) *InternalError {
+	return &InternalError{
+		Message: message,
+		Err:     "internal_server_error",
+	}
+}
