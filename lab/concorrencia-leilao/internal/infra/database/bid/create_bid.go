b48ca66e0352396bd4fa568752dfefcafc7f30fe
diogokimisima <dkimisima@gmail.com>
2026-01-05 15:11:56 -0300

    feat: add bid entity and repository with create and find operations

---
+++ lab/concorrencia-leilao/internal/infra/database/bid/create_bid.go
@@ -0,0 +1,67 @@
+package bid
+
+import (
+	"context"
+	"sync"
+
+	"github.com/diogokimisima/fullcycle-auction/configuration/logger"
+	"github.com/diogokimisima/fullcycle-auction/internal/entity/auction_entity"
+	"github.com/diogokimisima/fullcycle-auction/internal/entity/bid_entity"
+	"github.com/diogokimisima/fullcycle-auction/internal/infra/database/auction"
+	"github.com/diogokimisima/fullcycle-auction/internal/internal_error"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type BidEntityMongo struct {
+	Id        string  `bson:"_id"`
+	UserId    string  `bson:"user_id"`
+	AuctionId string  `bson:"auction_id"`
+	Amount    float64 `bson:"amount"`
+	Timestamp int64   `bson:"timestamp"`
+}
+
+type BidRepository struct {
+	Collection        *mongo.Collection
+	AuctionRepository *auction.AuctionRepository
+}
+
+func (bd *BidRepository) CreateBid(
+	ctx context.Context,
+	bidEntities []*bid_entity.Bid) *internal_error.InternalError {
+	var wg sync.WaitGroup
+
+	for _, bid := range bidEntities {
+		wg.Add(1)
+
+		go func(bidValue bid_entity.Bid) {
+			defer wg.Done()
+
+			auctionEntity, err := bd.AuctionRepository.FindAuctionById(ctx, bidValue.AuctionId)
+
+			if err != nil {
+				logger.Error("Error trying to find auction for bid creation", err)
+				return
+			}
+
+			if auctionEntity.Status != auction_entity.Active {
+				return
+			}
+
+			bidEntityMongo := &BidEntityMongo{
+				Id:        bidValue.Id,
+				UserId:    bidValue.UserId,
+				AuctionId: bidValue.AuctionId,
+				Amount:    bidValue.Amount,
+				Timestamp: bidValue.Timestamp.Unix(),
+			}
+
+			if _, err := bd.Collection.InsertOne(ctx, bidEntityMongo); err != nil {
+				logger.Error("Error trying to insert bid", err)
+				return
+			}
+		}(*bid)
+	}
+
+	wg.Wait()
+	return nil
+}
