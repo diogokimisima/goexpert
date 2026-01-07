f31e3fc414854f76b18c1f521e0f970842cea6e6
diogokimsiima <dkimisima@gmail.com>
2026-01-07 17:59:37 -0300

    feat: add auction status update functionality and configurable auction interval

--- lab/concorrencia-leilao/internal/infra/database/auction/create_auction.go
+++ lab/concorrencia-leilao/internal/infra/database/auction/create_auction.go
@@ -2,9 +2,13 @@ package auction
 
 import (
 	"context"
+	"os"
+	"time"
 
+	"github.com/diogokimisima/fullcycle-auction/configuration/logger"
 	"github.com/diogokimisima/fullcycle-auction/internal/entity/auction_entity"
 	"github.com/diogokimisima/fullcycle-auction/internal/internal_error"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -48,5 +52,28 @@ func (ar *AuctionRepository) CreateAuction(
 			"error trying to create auction: " + err.Error())
 	}
 
+	go func() {
+		time.Sleep(getAuctionInterval())
+		update := bson.M{"$set": bson.M{"status": auction_entity.Completed}}
+		filter := bson.M{"_id": auction.Id}
+
+		_, err := ar.Collection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			logger.Error("error trying to update auction status: ", err)
+			return
+		}
+	}()
+
 	return nil
+}
+
+func getAuctionInterval() time.Duration {
+	auctionInverval := os.Getenv("AUCTION_INTERVAL")
+	duration, err := time.ParseDuration(auctionInverval)
+
+	if err != nil {
+		return time.Minute * 5
+	}
+
+	return duration
 }
